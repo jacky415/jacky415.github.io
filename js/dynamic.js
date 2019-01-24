728bfd0e1b86a33841dc66185200964082cf7ba9
Jacky Hua <jackyh1210sf@gmail.com>
2019-01-23 18:11:48 -0800

    First Website Upload

---
+++ js/dynamic.js
@@ -0,0 +1,20 @@
+* { 
+	color: black;
+}
+div.a {
+	width: 50%;
+    height: 300px;
+    background: url("https://images.unsplash.com/photo-1544077960-604201fe74bc?ixlib=rb-	  1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80") green;
+    background-size: contain;
+    text-align: center;
+}
+
+div.a p {
+    background: lightblue;
+	margin: 25% 0;
+}
+
+<div class="a">
+		<h2>text-align: center:</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut.</p>
+</div>
